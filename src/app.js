0d6277d638d582bc428fb31557abccf836112329
Dolev Pearl <19dolevp@gmail.com>
2020-10-03 02:11:21 +0300

    Simple express.js app with raw data from file JSON response

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-const http = require('http');
+const express = require('express')
+const csvtojson = require('csvtojson')
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const app = express()
+const port = 8080
+const csvFilePath = './data/covid_statistics.csv'
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+let covidCasesData
+const retrieveCovidData = async () => csvtojson({ ignoreEmpty: true }).fromFile(csvFilePath)
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', async (req, res) => {
+  covidCasesData = covidCasesData || await retrieveCovidData()
+  res.json(covidCasesData)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
