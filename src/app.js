1dbedd87a22830c1f4b269e63b8ecc92e76af613
Dolev Pearl <19dolevp@gmail.com>
2020-10-03 21:51:05 +0300

    use cors to allow data fetching

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const statisticsRouter = require('./routes/statistics')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/statistics', statisticsRouter)
