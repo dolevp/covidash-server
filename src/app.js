38062537061129c88a26f1f792f23d7cc63d2bfe
Dolev Pearl <19dolevp@gmail.com>
2020-10-03 03:31:11 +0300

    Partially functioning fetching from remote github repository

--- src/app.js
+++ src/app.js
@@ -1,16 +1,46 @@
 const express = require('express')
+const axios = require('axios')
 const csvtojson = require('csvtojson')
+const constants = require('./constants')
 
 const app = express()
 const port = 8080
 const csvFilePath = './data/covid_statistics.csv'
 
 let covidCasesData
-const retrieveCovidData = async () => csvtojson({ ignoreEmpty: true }).fromFile(csvFilePath)
+const zeroPadInteger = (element, targetLength = 2) => {
+  let paddedElement = element
+  while (paddedElement.length < targetLength) {
+    paddedElement = `0${paddedElement}`
+  }
+
+  return paddedElement
+}
+const getFormattedTodayStr = () => {
+  const today = new Date()
+  const month = zeroPadInteger(today.getUTCMonth() + 1) // +1 because it's starting from 0
+  const day = zeroPadInteger(today.getUTCDate())
+  const year = today.getUTCFullYear()
+  return `${month}-${day}-${year}`
+}
+const retrieveCovidData = async () => {
+  const file = await axios({
+    url: `${constants.covidStatisticsGithubRepositoryURL}/${getFormattedTodayStr()}.csv`,
+    method: 'GET',
+    responseType: 'stream',
+  })
+  return csvtojson({ ignoreEmpty: true })
+    .fromStream(file)
+}
+
 
 app.get('/', async (req, res) => {
-  covidCasesData = covidCasesData || await retrieveCovidData()
-  res.json(covidCasesData)
+  try {
+    covidCasesData = covidCasesData || await retrieveCovidData()
+    return res.json(covidCasesData)
+  } catch (e) {
+    return res.status(500).send(e)
+  }
 })
 
 app.listen(port, () => {
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+  covidStatisticsGithubRepositoryURL: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports',
+}
